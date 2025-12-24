ff4f8eb65730217947a0f1240caf9221f6547792
vitorbelarmino <vitor.belarmino@hotmail.com>
2025-12-24 13:16:19 -0300

    feat: refactor add routes, remove config method and add cors package

--- App.ts
+++ App.ts
@@ -1,31 +1,16 @@
 import express from "express";
-import "express-async-errors";
+import cors from "cors";
 import GlobalError from "./src/middlewares/GlobalError";
+import { routes } from "./src/routes";
 
 export class App {
   public app: express.Application;
   constructor() {
     this.app = express();
-    this.config();
+    this.app.use(cors());
     this.app.use(express.json());
-    this.app.get("/", (req, res) => {
-      res.send("Hello, World!");
-    });
+    this.app.use("/", routes);
     this.app.use(GlobalError.handleError);
-  }
-
-  private config(): void {
-    const accessControl: express.RequestHandler = (_req, res, next): void => {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,OPTIONS,PUT,PATCH");
-      res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-      if (_req.method === "OPTIONS") {
-        res.status(200).end();
-        return;
-      }
-      next();
-    };
-    this.app.use(accessControl);
   }
 
   public start(port: number | string): void {
